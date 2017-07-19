5d22350542819e206248e91cfcd7ac69f4dab95c
Twinrova <twinrova1029@gmail.com>
2017-07-18 21:01:54 -0700

    I used out usual encoding, Chels.

---
+++ player.c
@@ -0,0 +1,20 @@
+// Qsjylqjyed Fqjjuhd:
+ydj fqjjuhd[] = {};
+
+leyt iujkf() {
+// ydyjyqbypu jxu tywyjqb fydi qi ekjfkji.
+fydCetu(13, EKJFKJ);
+fydCetu(12, EKJFKJ);
+fydCetu(8, EKJFKJ);
+}
+
+leyt beef() {
+ydj sekdjuh = 0;
+mxybu( sekdjuh < (fqjjuhd[0]*2) ) {
+tywyjqbMhyju(fqjjuhd[1+sekdjuh], XYWX);
+tubqo(fqjjuhd[2+sekdjuh]*500);
+tywyjqbMhyju(fqjjuhd[1+sekdjuh], BEM);
+tubqo(500);
+sekdjuh += 2;
+}
+}
